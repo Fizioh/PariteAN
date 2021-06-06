b4dc170971dc1d4b043e850e824d443369d7f87b
Fizioh <batrom@laposte.net>
2021-06-06 19:24:24 +0200

    special methods

--- parite.py
+++ parite.py
@@ -11,12 +11,19 @@ lg.basicConfig(level=lg.DEBUG)
 
 def parse_arguments():
     parser = argparse.ArgumentParser()
-    
-    parser.add_argument("-d","--datafile",help="""CSV file containing pieces of 
+    parser.add_argument("-d","--datafile",help="""CSV file containing pieces of
         information about the members of parliament""")
     parser.add_argument("-e", "--extension", help="""Kind of file to analyse. Is it a CSV or an XML?""")
-    parser.add_argument("-p","--byparty",action='store_true',help="displays a graph for each political party")
-    
+    parser.add_argument("-p","--byparty", action='store_true', help="""displays
+        a graph for each political party""")
+    parser.add_argument("-i","--info", action='store_true', help="""information about
+        the file""")
+    parser.add_argument("-n","--displaynames", action='store_true', help="""displays
+        the names of all the mps""")
+    parser.add_argument("-s","--searchname", help="""search for a mp name""")
+    parser.add_argument("-I","--index", help="""displays information about the Ith mp""")
+    parser.add_argument("-g","--groupfirst", help="""displays a graph groupping all the 'g'
+        biggest political parties""")
     return parser.parse_args()
 
 def main():
@@ -31,7 +38,8 @@ def main():
         if args.extension == 'xml':
             x_an.launch_analysis(datafile)
         elif args.extension == 'csv':
-            c_an.launch_analysis(datafile, args.byparty)
+            c_an.launch_analysis(datafile, args.byparty, args.info, args.displaynames,
+                           args.searchname, args.index, args.groupfirst)
     finally:
         lg.info('#################### Analysis is over ######################')
 
